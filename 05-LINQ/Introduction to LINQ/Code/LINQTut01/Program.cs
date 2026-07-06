b1a04f700001df9d24ccc28d783260347b9bcc99
menna <redamenna368@gmail.com>
2026-07-06 16:00:17 +0300

    modified demo project

---
+++ 05-LINQ/Introduction to LINQ/Code/LINQTut01/Program.cs
@@ -0,0 +1,23 @@
+﻿using FunctionalProgramming;
+using System;
+namespace LINQTut01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+            var femaleWithFNameStartsWiths01 = employees
+                .Filter(e => e.Gender == "female" && e.FirstName.ToLowerInvariant().StartsWith("s"));
+
+            femaleWithFNameStartsWiths01.Print("female With FName Starts With s / Filter");
+
+
+            var femaleWithFNameStartsWiths02 = employees
+                .Where(e => e.Gender == "female" && e.FirstName.ToLowerInvariant().StartsWith("s"));
+
+            femaleWithFNameStartsWiths02.Print("female With FName Starts With s / Where");
+
+        }
+    }
+}
---
+++ 05-LINQ/Introduction to LINQ/Code/LINQTut02/Program.cs
@@ -0,0 +1,60 @@
+﻿using FunctionalProgramming;
+using System.Collections;
+
+namespace LINQTut02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var collection = new ArrayList { 1, true, "menna",DateTime.Now };
+
+            var evennumbers = numbers.Where(x => x % 2 == 0);
+
+            numbers.Add(10);
+            numbers.Add(12);
+            numbers.Remove(4);
+
+            foreach (var n in evennumbers)
+            {
+                Console.Write($" {n} ");
+            }
+            // 2 4 6 8 or 2  6  8  10  12
+            // 2  6  8  10  12
+            //  مش بتتنفذ غير لما ابدا اعمل اللوبwhere جملة -> deferred execution
+            // deferred execution: it will be executed when iterating in a loop
+            // or when the collection will be iterated in some other way.
+
+            // مقدرش اطبق عليها ال LINQ
+            // لازم يكون IEnumerable<T>
+            //var blabla = collection.Where(x => x % 2 == 0);
+
+            //الطرق المختلف اللي ممكن اكتب بيها الكويري
+
+            var evenNumbersUsingExtensionWhere =
+                numbers.Where(x => x % 2 == 0);
+
+            var evenNumbersUsingEnumerableWhereMethod =
+                Enumerable.Where(numbers, x => x % 2 == 0);
+
+            var evenNumbersUsingQuerySyntax =
+                from n in numbers
+                where n % 2 == 0
+                select n;
+
+            //  Enumerable.Where(numbers, x => x % 2 == 0); كلهم بيترجموا الى 
+            evenNumbersUsingExtensionWhere.Print("evenNumbersUsingExtensionWhere");
+            evenNumbersUsingEnumerableWhereMethod.Print("evenNumbersUsingEnumerableWhereMethod");
+            evenNumbersUsingQuerySyntax.Print("evenNumbersUsingQuerySyntax");
+
+
+        }
+
+
+
+    }
+    
+    
+}
