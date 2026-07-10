f2267f96e076da5904b2be2497a2241487123d8f
menna <redamenna368@gmail.com>
2026-07-10 22:12:57 +0300

    add LINQTut07 solution

---
+++ 05-LINQ/Grouping data/LINQTut07/LINQTut07.GroupingData/Program.cs
@@ -0,0 +1,45 @@
+﻿using LINQTut07.Shared;
+
+namespace LINQTut07.GroupingData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunGroupBy();
+            RunToLookup();
+        }
+        private static void RunGroupBy()
+        {
+            var emps = Repository.LoadEmployees();
+            // Method syntax
+            var groups = emps.GroupBy(x => x.Department);
+            foreach (var group in groups)
+            {
+                group.Print($"Employees in Department: {group.Key}");
+            }
+
+            // Query syntax
+            var groups02 = from e in emps
+                           group e by e.Department;
+            foreach (var group in groups02)
+            {
+                group.Print($"Employees in Department: {group.Key}");
+            }
+
+        }
+
+        private static void RunToLookup()
+        {
+            var emps = Repository.LoadEmployees();
+           
+            var groups = emps.ToLookup(x => x.Department);
+            foreach (var group in groups)
+            {
+                group.Print($"Employees in Department: {group.Key}");
+            }
+
+            
+        }
+    }
+}
