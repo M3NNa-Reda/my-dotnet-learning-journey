ffdc73ce8e17d45656006643f420b36d1744bcbc
menna <redamenna368@gmail.com>
2026-07-28 22:37:17 +0300

    add LINQTut09 solution

---
+++ 05-LINQ/Generation Operations/LINQTut09/Shared/Choice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class Choice
+    {
+        public int Order { get; set; }
+        public string Description { get; set; }
+    }
+}
